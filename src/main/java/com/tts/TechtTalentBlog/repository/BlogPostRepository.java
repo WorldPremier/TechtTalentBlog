1129493b04640074d7f41fc8006dd68dd5b91ee5
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-05-20 10:48:35 -0400

    Initial commit

---
+++ src/main/java/com/tts/TechtTalentBlog/repository/BlogPostRepository.java
@@ -0,0 +1,10 @@
+package com.tts.TechtTalentBlog.repository;
+
+import com.tts.TechtTalentBlog.model.BlogPost;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+}
